7bd1eebf5d6bc955740350560ace3bb056235feb
Aliaksandr Karpovich <alexsure.k@gmail.com>
2015-02-11 14:21:13 +0300

    Chat image sharing

--- app/controllers/Chat.go
+++ app/controllers/Chat.go
@@ -5,6 +5,8 @@ import (
 	"github.com/revel/revel"
 	"gogo/app/chat"
 	"gogo/app/models"
+	"os"
+	"io"
 )
 
 type Chat struct {
@@ -16,46 +18,80 @@ func (this Chat) Room() revel.Result {
 }
 
 func (this Chat) RoomSocket(ws *websocket.Conn) revel.Result {
-	subscription := chat.Subscribe()
-	defer subscription.Cancel()
-
-	user := this.RenderArgs["loggedInUser"].(*models.User)
-
-	chat.Join(user.FirstName + " " + user.LastName)
-	defer chat.Leave(user.FirstName + " " + user.LastName)
-
-	for _, event := range subscription.Archive {
-		if websocket.JSON.Send(ws, &event) != nil {
-			return nil
-		}
-	}
-
-	newMessages := make(chan string)
-
-	go func() {
-		var msg string
-		for {
-			err := websocket.Message.Receive(ws, &msg)
-			if err != nil {
-				close(newMessages)
-				return
+	if this.Request.Method == "POST" {
+		result := make(map[string][]string)
+		fileList := make([]string,0)
+		m := this.Request.MultipartForm
+	    for fname, _ := range m.File {
+			fheaders := m.File[fname]
+			for i, _ := range fheaders {
+				//for each fileheader, get a handle to the actual file
+				file, err := fheaders[i].Open()
+				defer file.Close() //close the source file handle on function return
+				if err != nil {
+					panic(err)
+				}
+				//create destination file making sure the path is writeable.
+				dst_path := "public/img/uploaded/"+fheaders[i].Filename
+				dst, err := os.Create(dst_path)
+				defer dst.Close() //close the destination file handle on function return
+				defer os.Chmod(dst_path, (os.FileMode)(0644)) //limit access restrictions
+				if err != nil {
+					panic(err)
+				}
+				//copy the uploaded file to the destination file
+				if _, err := io.Copy(dst, file); err != nil {
+					panic(err)
+				}
+				fileList = append(fileList, dst_path)
 			}
-			newMessages <- msg
-		}
-	}()
+			revel.TRACE.Printf("Upload successful..")
+		}	
+		result["images"] = fileList
+		return this.RenderJson(result)
+	} else {
 
-	for {
-		select {
-		case event := <-subscription.New:
+		subscription := chat.Subscribe()
+		defer subscription.Cancel()
+
+		user := this.RenderArgs["loggedInUser"].(*models.User)
+
+		chat.Join(user.FirstName + " " + user.LastName)
+		defer chat.Leave(user.FirstName + " " + user.LastName)
+
+		for _, event := range subscription.Archive {
 			if websocket.JSON.Send(ws, &event) != nil {
 				return nil
 			}
-		case msg, ok := <-newMessages:
-			if !ok {
-				return nil
-			}
-			chat.Say(user.FirstName+" "+user.LastName, msg)
 		}
-	}
+
+		newMessages := make(chan string)
+
+		go func() {
+			var msg string
+			for {
+				err := websocket.Message.Receive(ws, &msg)
+				if err != nil {
+					close(newMessages)
+					return
+				}
+				newMessages <- msg
+			}
+		}()
+
+		for {
+			select {
+			case event := <-subscription.New:
+				if websocket.JSON.Send(ws, &event) != nil {
+					return nil
+				}
+			case msg, ok := <-newMessages:
+				if !ok {
+					return nil
+				}
+				chat.Say(user.FirstName+" "+user.LastName, msg)
+			}
+		}
+	}	
 	return nil
 }
